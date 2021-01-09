c4df80f842326b58661b9dc94875289f7c050aa3
kangalioo <jannik.a.schaper@web.de>
2021-01-09 12:15:01 +0100

    Add more functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #![no_std]
+#![warn(clippy::indexing_slicing)]
 
 //! This library provides several useful constructs to format data in a human-readable fashion with
 //! zero allocations
 //! 
-//! Some of these functions seem to partly reinvent existing std functionality, for example
+//! Some of these functions may seem to partly reinvent existing std functionality, for example
 //! [`join`]:
 //! 
 //! ```rust
-//! # use display_utils::*;
 //! println!("{}", display_utils::join(&[1, 2, 3], " + ")); // display_utils
 //! println!("{}", ["1", "2", "3"].join(" + ")); // std
 //! 
@@ -18,8 +18,9 @@
 //! The important difference is that the straightforward std way involves 4 allocations, whereas
 //! the display_utils way operates 100% on stack and is therefore no_std compatible and much faster.
 
-/// Print a loading-style bar using the Unicode block characters. The bar is very high-resolution:
-/// 8 states can be represented per character.
+/// Print a loading-style bar using Unicode block characters.
+/// 
+/// The bar is very high-resolution: 8 states can be represented per character.
 /// 
 /// Accepts the total length of the bar and a float from 0.0 to 1.0 as the filled proportion.
 /// 
@@ -84,6 +85,7 @@ pub fn unicode_block_bar(max_length: usize, proportion: f32) -> impl core::fmt::
 			num_spaces: 0,
 		}
 	} else {
+		#[allow(clippy::indexing_slicing)] // index will be in 0..8 always due to modulo
 		UnicodeBlockBar {
 			num_full_blocks: steps / 8,
 			midpoint: &BLOCK_CHARS[steps % 8],
@@ -92,6 +94,12 @@ pub fn unicode_block_bar(max_length: usize, proportion: f32) -> impl core::fmt::
 	}
 }
 
+/// Print a sequence of equalizer-style vertical bars using Unicode block characters.
+/// 
+/// The bars are very high-resolution: 8 states can be represented per character.
+/// 
+/// Accepts the total maximum height of the bars and an iterator over each bar's fill percentage.
+/// 
 /// ```rust
 /// let expected_output = "\
 /// █          
@@ -122,7 +130,7 @@ where
 	I::IntoIter: Clone,
 {
 	// index x = x 8ths of a full block
-	const BLOCK_CHARS: &[&str] = &[" ", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"];
+	const BLOCK_CHARS: [&str; 9] = [" ", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"];
 
 	struct VerticalUnicodeBlockBars<I> {
 		max_height: usize,
@@ -144,6 +152,7 @@ where
 
 					f.write_str(match row.cmp(&(steps / 8)) {
 						core::cmp::Ordering::Less => &BLOCK_CHARS[0],
+						#[allow(clippy::indexing_slicing)] // that index will always be in 0..=8
 						core::cmp::Ordering::Equal => &BLOCK_CHARS[8 - steps % 8],
 						core::cmp::Ordering::Greater => &BLOCK_CHARS[8],
 					})?;
@@ -158,7 +167,9 @@ where
 
 /// Concatenate iterator elements, separating each element pair with a given joiner.
 /// 
-/// The returned Display object is meant for immediate consumption
+/// Equivalent to [`slice::join`](https://doc.rust-lang.org/std/primitive.slice.html#method.join).
+/// 
+/// The iterator must be cloneable, because Display objects may be printed multiple times.
 /// 
 /// ```rust
 /// # use display_utils::*;
@@ -205,8 +216,11 @@ where
 	Join { iterator: iterator.into_iter(), joiner }
 }
 
-/// Concatenate iterator elements, separating each element pair with a given joiner. Each iterator
-/// element can be formatted using a callback.
+/// Concatenate iterator elements, separating each element pair with a given joiner, where each
+/// iterator element can be formatted using a callback.
+/// 
+/// The callback must be Fn and the iterator must be cloneable, because Display objects may be
+/// printed multiple times.
 /// 
 /// ```rust
 /// # use display_utils::*;
@@ -222,40 +236,43 @@ where
 pub fn join_format<I, C, J>(iterator: I, callback: C, joiner: J) -> impl core::fmt::Display
 where
 	I: IntoIterator,
-	C: FnMut(I::Item, &mut core::fmt::Formatter) -> core::fmt::Result,
+	I::IntoIter: Clone,
+	C: Fn(I::Item, &mut core::fmt::Formatter) -> core::fmt::Result,
 	J: core::fmt::Display,
 {
 	struct Join<I, C, J> {
-		iterator_and_callback: core::cell::RefCell<(I, C)>,
+		iterator: I,
+		callback: C,
 		joiner: J,
 	}
 
 	impl<I, C, J> core::fmt::Display for Join<I, C, J>
 	where
-		I: Iterator,
-		C: FnMut(I::Item, &mut core::fmt::Formatter) -> core::fmt::Result,
+		I: Iterator + Clone,
+		C: Fn(I::Item, &mut core::fmt::Formatter) -> core::fmt::Result,
 		J: core::fmt::Display,
 	{
 		fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-			let mut ref_guard = self.iterator_and_callback.borrow_mut();
-			let (iter, callback) = &mut *ref_guard;
+			let mut iter = self.iterator.clone();
 
 			if let Some(first_item) = iter.next() {
-				(callback)(first_item, f)?;
+				(self.callback)(first_item, f)?;
 			}
 			for remaining_item in iter {
 				self.joiner.fmt(f)?;
-				(callback)(remaining_item, f)?;
+				(self.callback)(remaining_item, f)?;
 			}
 
 			Ok(())
 		}
 	}
 	
-	Join { iterator_and_callback: core::cell::RefCell::new((iterator.into_iter(), callback)), joiner }
+	Join { iterator: iterator.into_iter(), callback, joiner }
 }
 
 /// Repeat an object a certain number of times.
+/// 
+/// Equivalent to `str::repeat`.
 /// 
 /// ```rust
 /// # use display_utils::*;
@@ -282,15 +299,155 @@ pub fn repeat<T: core::fmt::Display>(token: T, times: usize) -> impl core::fmt::
 	Repeat { token, times }
 }
 
-/// Indent to a given depth using the tab character. This is a shortcut for `repeat("\t", depth)`;
-/// please see that function if you want to use a different indent string.
+/// Indent to a given depth using the tab character.
+/// 
+/// This is a shortcut for `repeat("\t", depth)`; please see that function if you wish to use a
+/// different indent string.
 /// 
 /// ```rust
 /// # use display_utils::*;
-/// assert_eq!(display_utils::indent(2).to_string(), "\t\t");
-/// # assert_eq!(display_utils::indent(0).to_string(), "");
-/// # assert_eq!(display_utils::indent(1).to_string(), "\t");
+/// assert_eq!(indent(2).to_string(), "\t\t");
+/// # assert_eq!(indent(0).to_string(), "");
+/// # assert_eq!(indent(1).to_string(), "\t");
 /// ```
 pub fn indent(depth: usize) -> impl core::fmt::Display {
 	repeat("\t", depth)
+}
+
+/// Print a Unicode-compliant lowercase version of the string.
+/// 
+/// Equivalent to `str::to_lowercase`.
+/// 
+/// ```rust
+/// # use display_utils::*;
+/// assert_eq!(lowercase("GRÜẞE JÜRGEN").to_string(), "grüße jürgen");
+/// ```
+pub fn lowercase(source: &str) -> impl core::fmt::Display + '_ {
+	struct AsciiLowercase<'a> {
+		source: &'a str,
+	}
+
+	impl<'a> core::fmt::Display for AsciiLowercase<'a> {
+		fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+			use core::fmt::Write;
+			for input_char in self.source.chars() {
+				for output_char in input_char.to_lowercase() {
+					f.write_char(output_char)?;
+				}
+			}
+			Ok(())
+		}
+	}
+
+	AsciiLowercase { source }
+}
+
+/// Print a Unicode-compliant uppercase version of the string.
+/// 
+/// Equivalent to `str::to_uppercase`.
+/// 
+/// ```rust
+/// # use display_utils::*;
+/// assert_eq!(uppercase("grüße jürgen").to_string(), "GRÜSSE JÜRGEN");
+/// ```
+pub fn uppercase(source: &str) -> impl core::fmt::Display + '_ {
+	struct AsciiUppercase<'a> {
+		source: &'a str,
+	}
+
+	impl<'a> core::fmt::Display for AsciiUppercase<'a> {
+		fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+			use core::fmt::Write;
+			for input_char in self.source.chars() {
+				for output_char in input_char.to_uppercase() {
+					f.write_char(output_char)?;
+				}
+			}
+			Ok(())
+		}
+	}
+
+	AsciiUppercase { source }
+}
+
+/// Replace instances of the `from` string with the `to` string.
+/// 
+/// Note: this function, contrary to its std equivalent
+/// [`str::replace`](https://doc.rust-lang.org/std/primitive.str.html#method.replace),
+/// does not support the Pattern API because that API is not yet stabilized.
+/// 
+/// ```rust
+/// # use display_utils::*;
+/// assert_eq!(replace("this is old", "old", "new").to_string(), "this is new");
+/// # assert_eq!(replace("", "aaaaa", "xinbuldfgh").to_string(), "");
+/// # assert_eq!(replace("old is this", "old", "new").to_string(), "new is this");
+/// ```
+// TODO: change `from` parameter type to Pattern, once that API is stabilized
+pub fn replace<'a>(source: &'a str, from: &'a str, to: &'a str) -> impl core::fmt::Display + 'a {
+	struct Replace<'a> {
+		source: &'a str,
+		from: &'a str,
+		to: &'a str,
+	}
+
+	impl<'a> core::fmt::Display for Replace<'a> {
+		fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+			let mut last_end = 0;
+			for (start, part) in self.source.match_indices(self.from) {
+				// UNWRAP: match_indices returns well-aligned indices
+				f.write_str(self.source.get(last_end..start).unwrap())?;
+				f.write_str(self.to)?;
+				last_end = start + part.len();
+			}
+			// UNWRAP: last_end is well-aligned still
+			f.write_str(self.source.get(last_end..).unwrap())?;
+			Ok(())
+		}
+	}
+
+	Replace { source, from, to }
+}
+
+/// Replace the first n instances of the `from` string with the `to` string.
+/// 
+/// Note: this function, contrary to its std equivalent
+/// [`str::replacen`](https://doc.rust-lang.org/std/primitive.str.html#method.replacen),
+/// does not support the Pattern API because that API is not yet stabilized.
+/// 
+/// ```rust
+/// # use display_utils::*;
+/// assert_eq!(replace_n("old old old", "old", "new", 2).to_string(), "new new old");
+/// # assert_eq!(replace_n("", "aaaaa", "xinbuldfgh", 987).to_string(), "");
+/// # assert_eq!(replace_n("old is this", "old", "new", 0).to_string(), "old is this");
+/// ```
+// TODO: change `from` parameter type to Pattern, once that API is stabilized
+pub fn replace_n<'a>(
+	source: &'a str,
+	from: &'a str,
+	to: &'a str,
+	n: usize
+) -> impl core::fmt::Display + 'a {
+	struct ReplaceN<'a> {
+		source: &'a str,
+		from: &'a str,
+		to: &'a str,
+		n: usize,
+	}
+
+	impl<'a> core::fmt::Display for ReplaceN<'a> {
+		fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+			let mut last_end = 0;
+			for (start, part) in self.source.match_indices(self.from).take(self.n) {
+				// UNWRAP: match_indices returns well-aligned indices
+				f.write_str(self.source.get(last_end..start).unwrap())?;
+				f.write_str(self.to)?;
+				last_end = start + part.len();
+			}
+			// UNWRAP: last_end is well-aligned still
+			f.write_str(self.source.get(last_end..).unwrap())?;
+			Ok(())
+		}
+	}
+
+	ReplaceN { source, from, to, n }
 }
